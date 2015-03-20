4f03df9446a6017bee07cb6d7f8ede77f7377c04
Michael van Engelshoven <michael@van-engelshoven.de>
2015-03-20 08:20:18 +0100

    Fix keyboardProvider redirects for better testability
    It's easier to test if we are able to mock the methods of
    $$keyboardParserProvider. But after mocking these methods, the will
    not be the same methods as on $keyboardProvider anymore. So we need
    to access the object properties on mehtod call.

--- src/KeyboardProvider.js
+++ src/KeyboardProvider.js
@@ -2,10 +2,21 @@ var KeyboardService = require('./KeyboardService');
 
 module.exports =  ['$$keyboardParserProvider', function KeyboardProvider($$keyboardParserProvider) {
 
-    this.addKeyMapping = $$keyboardParserProvider.addKeyMapping;
-    this.setKeyMappings = $$keyboardParserProvider.setKeyMappings;
-    this.addMacro = $$keyboardParserProvider.addMacro;
-    this.setMacros = $$keyboardParserProvider.setMacros;
+    this.addKeyMapping = function() {
+        $$keyboardParserProvider.addKeyMapping.apply($$keyboardParserProvider, arguments);
+    };
+
+    this.setKeyMappings = function() {
+        $$keyboardParserProvider.setKeyMappings.apply($$keyboardParserProvider, arguments);
+    };
+
+    this.addMacro = function() {
+        $$keyboardParserProvider.addMacro.apply($$keyboardParserProvider, arguments);
+    };
+
+    this.setMacros = function() {
+        $$keyboardParserProvider.setMacros.apply($$keyboardParserProvider, arguments);
+    };
 
     this.$get = ['$injector', function($injector) {
         var keyboard = $injector.instantiate(KeyboardService);
