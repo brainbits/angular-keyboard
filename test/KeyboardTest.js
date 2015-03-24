edfad8fb097910269dd5eb6445dae6e1c5b3cfb3
Michael van Engelshoven <michael@van-engelshoven.de>
2015-03-24 07:16:26 +0100

    Add tests for $keyboardProvider

--- test/KeyboardTest.js
+++ test/KeyboardTest.js
@@ -4,32 +4,66 @@ describe('$keyboard', function() {
 
     describe('configuring the parser', function() {
 
-        var $$keyboardParserProvider,
-            $keyboardProvider;
+        var $$keyboardParserProvider;
 
-        beforeEach(module(function(_$$keyboardParserProvider_, _$keyboardProvider_) {
+        beforeEach(module(function(_$$keyboardParserProvider_) {
             $$keyboardParserProvider = _$$keyboardParserProvider_;
-            $keyboardProvider = _$keyboardProvider_;
-            sinon.stub($$keyboardParserProvider, 'setKeyMappings');
-            sinon.stub($$keyboardParserProvider, 'setMacros');
+            sinon.stub($$keyboardParserProvider);
         }));
 
-        it('passes mappings to $$keyboardParserProvider', function() {
+        it('passes calls of setKeyMappings to $$keyboardParserProvider', function() {
+            var expectedMappings = { 47: 'a', 11: 'b' };
 
-            module(function(_$keyboardProvider_) {
-                $keyboardProvider.setKeyMappings({
-                    65: 'a'
-                });
-                $keyboardProvider.setMacros({
-                });
+            module(function($keyboardProvider) {
+                $keyboardProvider.setKeyMappings(expectedMappings);
             });
 
             inject(function() {
-                expect($$keyboardParserProvider.setKeyMappings).to.be.called;
+                expect($$keyboardParserProvider.setKeyMappings).to.be.calledOnce
+                    .and.calledWith(expectedMappings);
             });
         });
 
+        it.skip('passes calls of setKeyMappings to $$keyboardParserProvider', function() {
+            var expectedMacros = {'a > b': ['foo']};
 
+            module(function($keyboardProvider) {
+                $keyboardProvider.setMacros(expectedMacros);
+            });
+
+            inject(function() {
+                expect($$keyboardParserProvider.setMacros).to.be.calledOnce
+                    .and.calledWith(expectedMacros);
+            });
+        });
+
+        it('passes calls of addKeyMapping to $$keyboardParserProvider', function() {
+            var expectedKeycode = 15,
+                expectedMappings = ['foo', 'bar'];
+
+            module(function($keyboardProvider) {
+                $keyboardProvider.addKeyMapping(expectedKeycode, expectedMappings);
+            });
+
+            inject(function() {
+                expect($$keyboardParserProvider.addKeyMapping).to.be.calledOnce
+                    .and.calledWith(expectedKeycode, expectedMappings)
+            });
+        });
+
+        it('passes calls of addMacro to $$keyboardParserProvider', function() {
+            var expectedCombo = 'ctrl+z',
+                expectedNames = ['undo'];
+
+            module(function($keyboardProvider) {
+                $keyboardProvider.addMacro(expectedCombo, expectedNames);
+            });
+
+            inject(function() {
+                expect($$keyboardParserProvider.addMacro).to.be.calledOnce
+                    .and.calledWith(expectedCombo, expectedNames)
+            });
+        });
     });
 
 });
